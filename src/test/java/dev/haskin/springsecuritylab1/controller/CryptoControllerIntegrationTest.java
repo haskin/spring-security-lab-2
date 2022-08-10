3bbfecc89135a1ef1fc8b269f2a93522b1e6a67a
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-09 22:18:34 -0400

    Added admin access to crypto integeration tests

--- src/test/java/dev/haskin/springsecuritylab1/controller/CryptoControllerIntegrationTest.java
+++ src/test/java/dev/haskin/springsecuritylab1/controller/CryptoControllerIntegrationTest.java
@@ -2,13 +2,15 @@ package dev.haskin.springsecuritylab1.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -19,8 +21,16 @@ public class CryptoControllerIntegrationTest {
     private MockMvc mockMvc;
 
     @Test
-    void shouldReturnCryptoFromName() throws Exception {
+    @WithMockUser(username = "fakeuser", authorities = "admin")
+    void shouldReturnBadRequestForInvalidCryptoName() throws Exception {
+        mockMvc.perform(get("/crypto/aldjfaosdjfojlwejasdjfoajsdf"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
 
+    @Test
+    @WithMockUser(username = "fakeuser", authorities = "admin")
+    void shouldReturnCryptoFromName() throws Exception {
         mockMvc.perform(get("/crypto/bitcoin"))
                 .andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
